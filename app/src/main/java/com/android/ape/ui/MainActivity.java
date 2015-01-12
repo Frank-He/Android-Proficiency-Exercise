efa22620d2d1bb13d2a9bab60cb442025a73164e
Zaidong <zaidong.he@rea-group.com>
2015-01-12 13:27:47 +0800

    Add RxJava

--- app/src/main/java/com/android/ape/ui/MainActivity.java
+++ app/src/main/java/com/android/ape/ui/MainActivity.java
@@ -16,6 +16,12 @@ import com.android.ape.network.FeedGetter;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 
 public class MainActivity extends ActionBarActivity implements SwipeRefreshLayout.OnRefreshListener {
@@ -32,6 +38,8 @@ public class MainActivity extends ActionBarActivity implements SwipeRefreshLayou
     private FeedListAdapter mFeedListAdapter;
 
     private FeedGetter mFeedGetter;
+
+    private Subscription mSubscription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,6 +86,8 @@ public class MainActivity extends ActionBarActivity implements SwipeRefreshLayou
     private void loadFeedFromServer() {
         FeedLoadTask feedLoadTask = new FeedLoadTask();
         feedLoadTask.execute((Void[]) null);
+
+        mSubscription = createFeedObservable().subscribe(mFeedObserver);
     }
 
     protected void updateActionbarTitle(String title) {
@@ -88,6 +98,9 @@ public class MainActivity extends ActionBarActivity implements SwipeRefreshLayou
         mFeedListAdapter.updateFeedData(feed);
     }
 
+    /*
+        The way of AsyncTask
+     */
     private class FeedLoadTask extends AsyncTask<Void, Void, Feed> {
 
         @Override
@@ -120,5 +133,45 @@ public class MainActivity extends ActionBarActivity implements SwipeRefreshLayou
                 updateActionbarTitle(feed.getTitle());
             }
         }
+    }
+
+    /*
+        The way of RxJava
+     */
+    private Observer<Feed> mFeedObserver = new Observer<Feed>() {
+        @Override
+        public void onCompleted() {
+            mSubscription.unsubscribe();
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            Toast.makeText(MainActivity.this, R.string.message_refresh_wrong, Toast.LENGTH_SHORT).show();
+            mSubscription.unsubscribe();
+        }
+
+        @Override
+        public void onNext(Feed feed) {
+            if (mSwipeRefreshLayout.isRefreshing()) {
+                // pull to refresh
+                mSwipeRefreshLayout.setRefreshing(false);
+            } else {
+                mProgressBar.setVisibility(View.GONE);
+            }
+
+            // Get feed success
+            updateFeedListContent(feed);
+            updateActionbarTitle(feed.getTitle());
+        }
+    };
+
+    private Observable<Feed> createFeedObservable() {
+        return Observable.create(new Observable.OnSubscribe<Feed>() {
+            @Override
+            public void call(Subscriber<? super Feed> subscriber) {
+                subscriber.onNext(mFeedGetter.getFromServer());
+                subscriber.onCompleted();
+            }
+        }).subscribeOn(Schedulers.newThread()).observeOn(AndroidSchedulers.mainThread());
     }
 }
